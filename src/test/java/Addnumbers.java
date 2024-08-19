17a5b3511dff00c65151c400d78fe5d35000e98a
kumar <gnaresh2546@gmail.com>
2024-08-19 14:26:42 +0530

    added

---
+++ src/test/java/Addnumbers.java
@@ -0,0 +1,13 @@
+
+public class Addnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 6;
+		int sum = x + y;
+		System.out.println(sum);
+
+	}
+
+}
